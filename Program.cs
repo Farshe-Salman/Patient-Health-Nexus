979a8691b435d73c044849f45ee05d75c19e7a58
Farshe-Salman <22-49844-3@student.aiub.edu>
2025-05-16 14:43:24 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Patient_Health_Nexus.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Patient_Health_Nexus
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            Application.Run(new Intro()); 
+        }
+    }
+}
